7316d38465e082fa3d60851cdaf65ebaa0202842
Sami Jakonen <sami.jakonen@iki.fi>
2021-02-13 23:26:22 +0200

    Create skeleton

---
+++ tempbodge_fetcher.py
@@ -0,0 +1,43 @@
+#      _       _        
+#  ___ (_) __ _| | _____  sjaks@github
+# / __|| |/ _` | |/ / __| jaks.fi
+# \__ \| | (_| |   <\__ \ ------------
+# |___// |\__,_|_|\_\___/ tempbodge
+#    |__/                
+#
+# BRIEF:
+# Reads the W1 temp sensor and sends 
+# values to cloud. Depends on:
+# apt-get install python3-w1thermsensor
+
+import requests
+import threading
+from w1thermsensor import W1ThermSensor, Unit
+
+
+TOKEN = "secret" # change to a secure password
+ENTRYPOINT = "https://jaks.fi/temperature/post"
+SENSOR = W1ThermSensor()
+
+
+# Helper for periodical function calls
+def setInterval(func, interval):
+    ev = threading.Event()
+    while not ev.wait(interval):
+        func()
+
+
+# Read temperature sensor and send value to entrypoint
+def send_temp():
+    temp = sensor.get_temperature()
+    print(temp)
+    payload = {"temps": temp, "token": TOKEN}
+    x = requests.post(ENTRYPOINT, data = payload)
+
+
+def main():
+    send_temp()
+    setInterval(send_temp, 600)
+
+
+main()
