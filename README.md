6f59a780238e21d0018bf711767ae78e93d02794
Sami Jakonen <sami.jakonen@iki.fi>
2021-04-06 00:04:20 +0300

    Use cron as scheduler

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ Very simple Raspberry Pi temperature monitor with a barebones frontend
 This is intended to be used as a base for developing your own temperature
 station/frontend combination. Change the secrets, urls and all that to
 fit your use case and use Apache or Nginx to point to the port 10990.
+
+On Raspberry Pi, use the following cron job, for example:
+```
+*/10 * * * * /usr/bin/python3 /home/sami/tempbodge/tempbodge_fetcher.py
+```
--- tempbodge_fetcher.py
+++ tempbodge_fetcher.py
@@ -8,14 +8,10 @@
 # BRIEF:
 # Reads the W1 temp sensor and sends
 # values to cloud. Depends on:
-#   apt-get install \
-#   python3-w1thermsensor \
-#   python3-schedule
+# apt-get install python3-w1thermsensor
 
 from datetime import datetime
 import requests
-import schedule
-import time
 from w1thermsensor import W1ThermSensor
 
 
@@ -24,17 +20,7 @@ ENTRYPOINT = "https://jaks.fi/temperature/post"
 SENSOR = W1ThermSensor()
 
 
-# Read temperature sensor and send value to entrypoint
-def send_temp():
-    temp = SENSOR.get_temperature()
-    curtime = datetime.now().strftime("%d/%m/%Y %H:%M")
-    payload = {"timestamp": curtime, "temp": temp, "secret": TOKEN}
-    print(payload)
-    req = requests.get(url = ENTRYPOINT, params = payload)
-
-
-# Init a scheduler and send temps every 5 minutes
-schedule.every(5).minutes.do(send_temp)
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+curtime = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+temp = SENSOR.get_temperature()
+payload = {"timestamp": curtime, "temp": temp, "secret": TOKEN}
+req = requests.get(url = ENTRYPOINT, params = payload)
