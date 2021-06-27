5431edb04971ee74fa6f98da645adb4f85097732
Sami Jakonen <sami.jakonen@iki.fi>
2021-06-27 11:25:55 +0300

    Streamline js and drop jQuery

--- server/static/chart.js
+++ server/static/chart.js
@@ -24,8 +24,7 @@ var temps = new Chart(tempsChart, {
         datasets: [
             {
                 // y-dataset for observed indoor temperatures
-                label: "Indoors",
-                data: [],
+                label: "Temperature",
                 lineTension: 0,
                 backgroundColor: "transparent",
                 borderColor: "#4cb5b5",
@@ -35,8 +34,6 @@ var temps = new Chart(tempsChart, {
             {
                 // y-dataset for day's maximum indoor temperature line,
                 // shows a horizontal line at the max temperature
-                label: "Indoors max",
-                data: [],
                 lineTension: 0,
                 backgroundColor: "transparent",
                 borderColor: "#e4629f",
@@ -47,8 +44,6 @@ var temps = new Chart(tempsChart, {
             {
                 // y-dataset for day's minimum indoor temperature line,
                 // shows a horizontal line at the min temperature
-                label: "Indoors min",
-                data: [],
                 lineTension: 0,
                 backgroundColor: "transparent",
                 borderColor: "#59b0e4",
@@ -61,11 +56,7 @@ var temps = new Chart(tempsChart, {
     options: {
         scales: {
             yAxes: [{
-                id: "Temperature",
-                position: 'left',
                 ticks: {
-                    // Use some intuitive max and min
-                    // values for the temperature y-axis
                     min: 14,
                     max: 34,
                     stepSize: 2
@@ -73,20 +64,20 @@ var temps = new Chart(tempsChart, {
             }],
             xAxes: [{
                 ticks: {
-                autoSkip: true,
-                maxTicksLimit: 20,
-                userCallback: function(label, index, labels) {
-                    // Strip down x-axis labels
-                    return label.substring(11,16)
-                },
+                    autoSkip: true,
+                    maxTicksLimit: 20,
+                    userCallback: function(label, index, labels) {
+                        // Strip down date from x-axis labels
+                        return label.substring(11,16)
+                    },
                 }
             }],
         },
         legend: {
-            display: false // legend not needed since there's just one relevant dataset
+            display: false
         },
         hover: {
-            mode: null // disables on-hover datapoint highlight
+            mode: null
         },
         tooltips: {
             yAlign: "top",
@@ -105,7 +96,7 @@ var temps = new Chart(tempsChart, {
                 },
                 label: function(tooltipItems, data) {
                     let label = data.datasets[tooltipItems.datasetIndex].label;
-                    if (label == "Indoors") {
+                    if (label == "Temperature") {
                         // Only show current temperatures in tooltip
                         return label + ": " + tooltipItems.yLabel.toFixed(2) + "°C";
                     }
@@ -116,55 +107,67 @@ var temps = new Chart(tempsChart, {
 });
 
 
+function getMinMaxTemp(arr) {
+    return [
+        parseFloat(Math.min.apply(Math, arr.map(function(el) { return el.temp; }))),
+        parseFloat(Math.max.apply(Math, arr.map(function(el) { return el.temp; })))
+    ];
+}
+
+
 // Function for getting new data and parsing it into the UI elements
 function updateData() {
     // Define path to backend data entrypoint.
     var url = "temperature/read";
 
-
     // Make GET request to backend
-    $.get(url, function(data) {
-        var iy = [], ix = [];
+    fetch(url).then(
+        function(response) {
+            response.json().then(function(data) {
+                var iy = [], ix = [];
 
-        // Place current, max and min temperatures into card views
-        currentTemperature.innerHTML = parseFloat(data[data.length - 1].temp).toFixed(2);
-        maxTemperature.innerHTML = parseFloat(Math.max.apply(Math, data.map(function(el) { return el.temp; }))).toFixed(2);
-        minTemperature.innerHTML = parseFloat(Math.min.apply(Math, data.map(function(el) { return el.temp; }))).toFixed(2);
+                // Place current, max and min temperatures into card views
+                var peakValues = getMinMaxTemp(data);
+                minTemperature.innerHTML = peakValues[0].toFixed(2);
+                maxTemperature.innerHTML = peakValues[1].toFixed(2);
+                currentTemperature.innerHTML = parseFloat(data[data.length - 1].temp).toFixed(2);
 
-        // Parse fetched data into arrays
-        for (var i = 0; i < data.length; i++) {
-            var date = new Date(0);
-            date.setUTCSeconds(data[i].timestamp);
-            date = date.toISOString().replace("T", " ").substring(0, 16);
-            ix.push(date);
-            iy.push(parseFloat(data[i].temp).toFixed(2));
+                // Parse fetched data into arrays
+                for (var i = 0; i < data.length; i++) {
+                    var date = new Date(0);
+                    date.setUTCSeconds(data[i].timestamp);
+                    date = date.toISOString().replace("T", " ").substring(0, 16);
+                    ix.push(date);
+                    iy.push(parseFloat(data[i].temp).toFixed(2));
+                }
+
+                // Show the previous data fetch time in top bar
+                updateTime.innerHTML = ix[ix.length - 1];
+
+                // Place observed temperatures into the graph
+                temps.data.labels = ix;
+                temps.data.datasets[0].data = iy;
+
+                // Create horizontal lines for max and min temperatures and draw them into the graph
+                var maxSeriesIn = Array(data.length);
+                var minSeriesIn = Array(data.length);
+                temps.data.datasets[1].data = maxSeriesIn.fill(peakValues[1], 0, data.length);
+                temps.data.datasets[2].data = minSeriesIn.fill(peakValues[0], 0, data.length);
+
+                // Show temperature trend arrow based on temperature delta
+                if (data[data.length - 1].temp >= data[data.length - 2].temp) {
+                    trendUp.style.display = "inline";
+                    trendDown.style.display = "none";
+                } else {
+                    trendDown.style.display = "inline";
+                    trendUp.style.display = "none";
+                }
+
+                // Redraw the graph
+                temps.update();
+            });
         }
-
-        // Show the previous data fetch time in top bar
-        updateTime.innerHTML = ix[ix.length - 1];
-
-        // Place observed temperatures into the graph
-        temps.data.labels = ix;
-        temps.data.datasets[0].data = iy;
-
-        // Create horizontal lines for max and min temperatures and draw them into the graph
-        var maxSeriesIn = Array(data.length);
-        var minSeriesIn = Array(data.length);
-        temps.data.datasets[1].data = maxSeriesIn.fill(maxTemperature.innerHTML, 0, data.length);
-        temps.data.datasets[2].data = minSeriesIn.fill(minTemperature.innerHTML, 0, data.length);
-
-        // Show temperature trend arrow based on temperature delta
-        if (data[data.length - 1].temp >= data[data.length - 2].temp) {
-            trendUp.style.display = "inline";
-            trendDown.style.display = "none";
-        } else {
-            trendDown.style.display = "inline";
-            trendUp.style.display = "none";
-        }
-
-        // Redraw the graph
-        temps.update();
-    });
+    )
 }
 
 
